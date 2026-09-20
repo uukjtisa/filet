b89ef8d0f19359789f5c21b8bcd95d62fb0151e4
Niccc2007 <94959895+uukjtisa@users.noreply.github.com>
2026-09-20 18:31:05 +0800

    chore(release): 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "dev.niccc2007.filet"
         minSdk = 26
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.1.7"
+        versionCode = 8
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
